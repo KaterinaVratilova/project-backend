deda45197b4d0d0270f28c69f38d869e6ba558a8
Tomas Svojanovsky <tomas.svojanovsky11@gmail.com>
2022-08-20 15:45:59 +0200

    Initial commit

---
+++ src/main/java/com/example/etorobackend/EtoroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.etorobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtoroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtoroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/etorobackend/EtoroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etorobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtoroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
